fc90cbadeb660ad7b43eabd74982381065bb0189
M4rioRms <26150481@aguascalientes.tecnm.mx>
2026-07-15 22:51:22 -0600

    Programa Promedio Tres Numeros

--- promedio_tres_numeros.py
+++ promedio_tres_numeros.py
@@ -1,4 +1,5 @@
-n1 = int(input("Dame el primer número: "))
-n2 = int(input("Dame el segundo número: "))
-n3 = int(input("Dame el tercer número: "))
-print("El promedio es:", (n1 + n2 + n3) // 3)
+n1 = float(input("Numero 1: "))
+n2 = float(input("Numero 2: "))
+n3 = float(input("Numero 3: "))
+promedio = (n1 + n2 + n3) / 3
+print("El promedio es:", )
