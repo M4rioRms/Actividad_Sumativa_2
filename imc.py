17ebeb7aeb2af9dd5d7c87d6cb3d9ff260894637
M4rioRms <26150481@aguascalientes.tecnm.mx>
2026-07-15 22:53:55 -0600

    Programa IMC

--- imc.py
+++ imc.py
@@ -1,4 +1,4 @@
-peso = int(input("Dime tu peso en kg: "))
-altura = int(input("Dime tu altura en cm: "))
-
-print("Tu IMC aproximado es:", (peso * 10000) // (altura ** 2))
+peso = float(input("Peso en kg: "))
+altura = float(input("Altura en m: "))
+imc = peso / (altura ** 2)
+print("Tu IMC es:", imc)
