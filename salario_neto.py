710d8bdf4c5bcb01d7c35b65dfccc99a5055e8ce
M4rioRms <26150481@aguascalientes.tecnm.mx>
2026-07-15 22:55:24 -0600

    Programa Salario neto

--- salario_neto.py
+++ salario_neto.py
@@ -1,2 +1,6 @@
-salario = int(input("Ingresa tu salario bruto: "))
-print("Tu salario neto es:", salario - (salario // 16))
+salario_bruto = float(input("Salario bruto: "))
+porcentaje = float(input("% impuestos: "))
+deducciones = float(input("Deducciones: "))
+impuesto = salario_bruto * (porcentaje / 100)
+salario_neto = salario_bruto - impuesto - deducciones
+print("Salario neto:", salario_neto)
