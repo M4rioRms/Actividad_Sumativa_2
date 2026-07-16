775766cece90ecbf1e353802dd37776dd03b0370
M4rioRms <26150481@aguascalientes.tecnm.mx>
2026-07-15 22:49:21 -0600

    Programa Area Rectangulo

--- area_rectangulo.py
+++ area_rectangulo.py
@@ -1,3 +1,4 @@
-base = int(input("Dame la base del rectángulo: "))
-altura = int(input("Dame la altura del rectángulo: "))
-print("El área es:", base * altura)
+base = float(input("Dame la base del rectángulo: "))
+altura = float(input("Dame la altura del rectángulo: "))
+area = base * altura
+print("El área es:", area)
